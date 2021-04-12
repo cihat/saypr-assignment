cf6512c6c4cbfe4bcb020dc0fd3a8e4b077f75ca
cihat <cihat02020202@gmail.com>
2021-04-12 20:29:43 +0300

    add style for user.vue component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
-  <div id="nav">
+  <!-- <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-  </div>
+  </div> -->
   <router-view />
 </template>
 
@@ -10,7 +10,7 @@
 @import url("https://fonts.googleapis.com/css2?family=Mulish:wght@400;500;700&display=swap");
 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Mulish", sans-serif, Helvetica, Arial;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/views/Users.vue
@@ -0,0 +1,31 @@
+<script>
+import User from "../components/User";
+export default {
+  name: "Home",
+  components: {
+    User,
+  },
+};
+</script>
+
+<template>
+  <div id="home">
+    <User />
+    <User />
+    <User />
+    <User />
+    <User />
+    <User />
+    <User />
+    <User />
+    <User />
+    <User />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+#home {
+  display: grid;
+  grid-template-columns: auto auto auto auto;
+}
+</style>
