91a14fcdd03c2b697e6c12db0cb96245a4b0b778
cihat <cihat02020202@gmail.com>
2021-04-12 22:21:42 +0300

    install  axios module and get data from api, configure data in page, add deploy.sh for gihub page

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,31 +1,47 @@
 <script>
 import User from "../components/User";
+import axios from "axios";
 export default {
   name: "Home",
+  data() {
+    return {
+      users: {},
+      userName: "",
+    };
+  },
   components: {
     User,
   },
+  async mounted() {
+    await axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        this.users = res.data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
+  methods: {},
 };
 </script>
 
 <template>
   <div id="home">
-    <User />
-    <User />
-    <User />
-    <User />
-    <User />
-    <User />
-    <User />
-    <User />
-    <User />
-    <User />
+    <ul>
+      <li v-for="(user, index) in users" :key="index">
+        <User :data="users[index]" :userName="users[index].name" />
+      </li>
+    </ul>
   </div>
 </template>
 
 <style lang="scss" scoped>
 #home {
-  display: grid;
-  grid-template-columns: auto auto auto auto;
+  ul {
+    display: grid;
+    // grid-template-columns: repeat(4, 1fr);
+    grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+  }
 }
 </style>
