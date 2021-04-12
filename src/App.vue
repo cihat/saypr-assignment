576c7b9119e8b3581448d3c5cf588b2f1967159d
cihat <cihat02020202@gmail.com>
2021-04-12 19:20:31 +0300

    add reset.css, webpack.config files and create empty scss files

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,12 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
-  <router-view/>
+  <router-view />
 </template>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Mulish:wght@400;500;700&display=swap");
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
